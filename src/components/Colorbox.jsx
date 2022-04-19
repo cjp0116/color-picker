1216bd7726d99ef743a6b36e154646cce1e99238
Jae Cho <jc960116@gmail.com>
2022-04-18 17:12:03 -0700

    added colorboxes css

--- src/components/Colorbox.jsx
+++ src/components/Colorbox.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./Colorbox.css";
 
-const Colorbox = (props) => {
+const Colorbox = ({ name, background }) => {
   return (
-    <div className="ColorBox" style={{ background: props.background }}>
-      <span>{props.name}</span>
-      <span>
-        MORE
-      </span>
+    <div className="ColorBox" style={{ background }}>
+      <div className="copy-container">
+        <div className="box-content">
+          <span>{name}</span>
+        </div>
+        <button className="copy-button">Copy</button>
+      </div>
+      <span className="see-more">More</span>
     </div>
   )
 }
