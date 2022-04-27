f7390f194a2d1787eadf3e0f656231a5caeef961
Jae Pil Cho <jc960116@gmail.com>
2022-04-27 13:54:45 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
 import Palette from './components/Palette';
 import seedColors from './seedColors';
+import { generatePallete } from './colorHelpers';
 
 function App() {
+  const test = generatePallete(seedColors[4])
+  console.log("🚀 ~ file: App.js ~ line 8 ~ App ~ test", test)
   return (
     <div className="App">
       <Palette palette={seedColors[4]} />
