672c8c932b2ed427d9c051cd525ec22bf8b68010
Jae Pil Cho <jc960116@gmail.com>
2022-04-27 15:50:03 -0700

    styled slider

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react'
 import Colorbox from './Colorbox';
-import "./Palette.css";
-
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import "./Palette.css";
 
 const Palette = ({ palette: { colors } }) => {
   const [level, setLevel] = useState(500)
   const changeLevel = (level) => { setLevel(level) }
   return (
     <div className="Palette">
-      <Slider step={100} defaultValue={level} min={100} max={900} onAfterChange={changeLevel} />
+      <div className="slider">
+      <Slider 
+        step={100}
+        defaultValue={level}
+        min={100}
+        max={900}
+        onAfterChange={changeLevel}
+      />
+      </div>
       {/* Navbar Goes here */}
       <div className="Palette-colors">
         {colors[level].map(({ hex, name }) => (
